9b930f884c330a6c0a9308335f7a2dbb7aba11bd
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-02-27 15:28:59 +0100

    fix(binary_trees): reorder typedefs and function prototypes

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -17,8 +17,8 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
