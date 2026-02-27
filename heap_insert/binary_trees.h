d40417720afc4f52b5ebb999307bbe2e5693d42a
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-02-27 15:26:15 +0100

    heat: add proto

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -17,6 +17,7 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
