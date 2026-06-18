eadff9dd435381e068bc54a75194e9cbc13c1a97
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-06-18 11:04:58 +0200

    feat: rotate 2D matrix in-place

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Rotate 2D matrix module.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate an n x n 2D matrix 90 degrees clockwise in-place.
+
+    Args:
+        matrix (list): Square 2D matrix to rotate.
+
+    Return:
+        None
+    """
+    size = len(matrix)
+
+    for row in range(size):
+        for col in range(row + 1, size):
+            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+
+    for row in range(size):
+        matrix[row].reverse()
