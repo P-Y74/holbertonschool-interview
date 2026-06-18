58349cf00fe6d7300a45e10be030efafab77f9cc
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-06-18 11:06:58 +0200

    fix: reduce line length for PEP8 validation

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -18,7 +18,8 @@ def rotate_2d_matrix(matrix):
 
     for row in range(size):
         for col in range(row + 1, size):
-            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+            matrix[row][col], matrix[col][row] = \
+                matrix[col][row], matrix[row][col]
 
     for row in range(size):
         matrix[row].reverse()
