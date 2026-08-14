3887a7dcca92153b31ac466b1fd1354190aab615
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2026-08-14 09:38:06 +0200

    feat: implement recursive wildcard string comparison

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings using wildcard matching
+ * @s1: first string
+ * @s2: second string, which may contain '*'
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+
+		return (wildcmp(s1, s2 + 1) ||
+				wildcmp(s1 + 1, s2));
+	}
+
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
